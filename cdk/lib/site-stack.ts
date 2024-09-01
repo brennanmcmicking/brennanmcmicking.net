85aa161cee69907560f4d9201f817b127bdad56d
Brennan McMicking <brennanmcmicking@gmail.com>
2024-08-31 22:08:50 -0700

    refactor!: site

--- cdk/lib/site-stack.ts
+++ cdk/lib/site-stack.ts
@@ -1,28 +1,40 @@
-import * as cdk from 'aws-cdk-lib';
-import { Certificate, CertificateValidation } from 'aws-cdk-lib/aws-certificatemanager';
-import { AllowedMethods, Distribution, OriginAccessIdentity, SecurityPolicyProtocol, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
-import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
-import { CanonicalUserPrincipal, PolicyStatement } from 'aws-cdk-lib/aws-iam';
-import { ARecord, HostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53';
-import { HttpsRedirect } from 'aws-cdk-lib/aws-route53-patterns';
-import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
-import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3';
-import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import {
+  Certificate,
+  CertificateValidation,
+} from "aws-cdk-lib/aws-certificatemanager";
+import {
+  AllowedMethods,
+  Distribution,
+  OriginAccessIdentity,
+  SecurityPolicyProtocol,
+  ViewerProtocolPolicy,
+} from "aws-cdk-lib/aws-cloudfront";
+import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
+import { CanonicalUserPrincipal, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { ARecord, HostedZone, RecordTarget } from "aws-cdk-lib/aws-route53";
+import { CloudFrontTarget } from "aws-cdk-lib/aws-route53-targets";
+import { BlockPublicAccess, Bucket } from "aws-cdk-lib/aws-s3";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import { Construct } from "constructs";
 
 export class SiteStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     const stage = props?.description;
-    const subDomain = stage === 'prod' ? 'www' : 'dev';
+    const subDomain = stage === "prod" ? "www" : "dev";
 
     const zone = HostedZone.fromLookup(this, `HostedZone-${stage}`, {
-      domainName: 'brennanmcmicking.net',
+      domainName: "brennanmcmicking.net",
     });
     const siteDomain = `${subDomain}.brennanmcmicking.net`;
-    const cloudfrontOAI = new OriginAccessIdentity(this, `cloudfront-OAI-${stage}`, {
-      comment: `OAI for ${id}`,
-    });
+    const cloudfrontOAI = new OriginAccessIdentity(
+      this,
+      `cloudfront-OAI-${stage}`,
+      {
+        comment: `OAI for ${id}`,
+      }
+    );
 
     const bucket = new Bucket(this, `SiteBucket-${stage}`, {
       bucketName: siteDomain,
@@ -31,11 +43,17 @@ export class SiteStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
-    bucket.addToResourcePolicy(new PolicyStatement({
-      actions: ['s3:GetObject'],
-      resources: [bucket.arnForObjects('*')],
-      principals: [new CanonicalUserPrincipal(cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId)],
-    }));
+    bucket.addToResourcePolicy(
+      new PolicyStatement({
+        actions: ["s3:GetObject"],
+        resources: [bucket.arnForObjects("*")],
+        principals: [
+          new CanonicalUserPrincipal(
+            cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
+          ),
+        ],
+      })
+    );
 
     const certificate = new Certificate(this, `SiteCertificate-${stage}`, {
       domainName: siteDomain,
@@ -44,24 +62,17 @@ export class SiteStack extends cdk.Stack {
 
     const distribution = new Distribution(this, `SiteDistribution-${stage}`, {
       certificate,
-      defaultRootObject: 'index.html',
+      defaultRootObject: "index.html",
       domainNames: [siteDomain],
       minimumProtocolVersion: SecurityPolicyProtocol.TLS_V1_2_2021,
-      // errorResponses: [
-      //   {
-      //     httpStatus: 403,
-      //     responseHttpStatus: 403,
-      //     responsePagePath
-      //   }
-      // ],
       defaultBehavior: {
         origin: new S3Origin(bucket, {
-          originAccessIdentity: cloudfrontOAI
+          originAccessIdentity: cloudfrontOAI,
         }),
         compress: true,
         allowedMethods: AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
         viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-      }
+      },
     });
 
     new ARecord(this, `SiteAliasRecord-${stage}`, {
@@ -71,10 +82,10 @@ export class SiteStack extends cdk.Stack {
     });
 
     new BucketDeployment(this, `BucketDeployment-${stage}`, {
-      sources: [ Source.asset("../frontend/build") ],
+      sources: [Source.asset("../www")],
       destinationBucket: bucket,
       distribution,
-      distributionPaths: ['/*']
+      distributionPaths: ["/*"],
     });
   }
 }
---
+++ www/main.js
@@ -0,0 +1,129 @@
+import * as THREE from "three";
+
+const fcElem = document.getElementById("frame-count");
+const rendererParent = document.getElementById("canvas-container");
+const screenSize = Math.min(
+  rendererParent.clientWidth,
+  rendererParent.clientHeight
+);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(25, 1.0, 0.1, 1000);
+let fc = 0;
+let rotVelX = 0;
+let rotVelY = 0;
+let rotVelZ = 0;
+let clicked = false;
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+cube.matrixAutoUpdate = false;
+cube.matrix = new THREE.Matrix4().makeRotationFromEuler(
+  new THREE.Euler(Math.PI / 4, Math.PI / 4, 0)
+);
+
+var light = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
+scene.add(light);
+
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer();
+console.log(rendererParent.clientWidth);
+renderer.setSize(screenSize, screenSize);
+// renderer.shadowMap.enabled = true;
+
+window.onresize = () => {
+  camera.updateProjectionMatrix();
+  const screenSize = Math.min(
+    rendererParent.clientWidth,
+    rendererParent.clientHeight
+  );
+  renderer.setSize(screenSize, screenSize);
+};
+
+function animate() {
+  cube.material.color = new THREE.Color(`hsl(${fc % 360}, 100%, 50%)`);
+
+  // https://stackoverflow.com/questions/37903979/set-an-objects-absolute-rotation-around-the-world-axis
+  var rotation = cube.matrix;
+  var worldXAxis = new THREE.Vector3(1, 0, 0).applyMatrix4(
+    rotation.clone().invert()
+  );
+  var rotationWorldX = new THREE.Matrix4().makeRotationAxis(
+    worldXAxis,
+    rotVelX
+  );
+  rotation.multiply(rotationWorldX);
+  var worldYAxis = new THREE.Vector3(0, 1, 0).applyMatrix4(
+    rotation.clone().invert()
+  );
+  var rotationWorldY = new THREE.Matrix4().makeRotationAxis(
+    worldYAxis,
+    rotVelY
+  );
+  rotation.multiply(rotationWorldY);
+  var worldZAxis = new THREE.Vector3(0, 0, 1).applyMatrix4(
+    rotation.clone().invert()
+  );
+  var rotationWorldZ = new THREE.Matrix4().makeRotationAxis(
+    worldZAxis,
+    rotVelZ
+  );
+  rotation.multiply(rotationWorldZ);
+
+  cube.matrix = rotation;
+
+  renderer.render(scene, camera);
+  fc++;
+  fcElem.innerText = fc;
+
+  if (rotVelX < -0.01) {
+    rotVelX += 0.01;
+  } else if (rotVelX > 0.01) {
+    rotVelX -= 0.01;
+  } else {
+    rotVelX = 0;
+  }
+
+  if (rotVelY < -0.01) {
+    rotVelY += 0.01;
+  } else if (rotVelY > 0.01) {
+    rotVelY -= 0.01;
+  } else {
+    rotVelY = 0;
+  }
+
+  if (rotVelZ < -0.01) {
+    rotVelZ += 0.01;
+  } else if (rotVelZ > 0.01) {
+    rotVelZ -= 0.01;
+  } else {
+    rotVelZ = 0;
+  }
+}
+
+renderer.setAnimationLoop(animate);
+document.getElementById("canvas-container").appendChild(renderer.domElement);
+
+document.onwheel = (e) => {
+  rotVel += e.deltaY / 10000;
+};
+
+document.body.onmousedown = (e) => {
+  clicked = true;
+};
+document.body.onmouseup = (e) => {
+  clicked = false;
+};
+document.body.onmouseleave = (e) => {
+  clicked = false;
+};
+document.body.onmousemove = (e) => {
+  if (clicked) {
+    rotVelX += e.movementY / 1000;
+    rotVelY += e.movementX / 1000;
+  }
+};
